965ed3c5753edd4eaa0e3f9f21969d0c8f09b90d
James Lee <james.lee56566@gmail.com>
2021-06-16 14:52:24 +0800

    2021-06-16 Practice cmake gcc and compiler instruction

---
+++ FunctionDemmo/cmakeQuickStart/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jameslee/program/2021/2021-leetcode/leetcode-code/FunctionDemmo/cmakeQuickStart
+# Build directory: /Users/jameslee/program/2021/2021-leetcode/leetcode-code/FunctionDemmo/cmakeQuickStart/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
